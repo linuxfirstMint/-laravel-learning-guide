96835f12cec6e0fc02232a28e6772f3ef4b468e9
flm_10001 <firstlinuxmint@gmail.com>
2025-12-29 11:04:50 +0000

    refactor: 静的コンテンツページをルートクロージャに変更
    シンプルな表示のみのページは専用Controllerが不要なため、
    ルートクロージャで直接Inertiaレスポンスを返すように変更。
    
    変更内容:
    - ErrorFixController、BladeEscapeController、RefactoringController、InertiaController を削除
    - routes/web.php で該当ルートをクロージャに置き換え
    - 不要なuseステートメントを削除
    
    メリット:
    - Controllerファイルが4つ減少
    - 新しい静的ページ追加時はJSXファイル作成とルート1行追加のみ
    - コードの見通しが良くなり保守性向上

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Http\Controllers\BladeEscapeController;
-use App\Http\Controllers\ErrorFixController;
-use App\Http\Controllers\InertiaController;
 use App\Http\Controllers\QueryBuilderController;
-use App\Http\Controllers\RefactoringController;
 use App\Http\Controllers\ValidationController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,7 +17,8 @@ Route::get('/validation', [ValidationController::class, 'index']);
 Route::post('/validation/validate', [ValidationController::class, 'validate']);
 Route::get('/validation/guide', [ValidationController::class, 'guide']);
 
-Route::get('/errorfix', [ErrorFixController::class, 'index']);
-Route::get('/bladeescape', [BladeEscapeController::class, 'index']);
-Route::get('/refactoring', [RefactoringController::class, 'index']);
-Route::get('/inertia', [InertiaController::class, 'index']);
+// 静的コンテンツページ（クロージャで簡潔に）
+Route::get('/errorfix', fn() => inertia('Errorfix/Index'));
+Route::get('/bladeescape', fn() => inertia('Bladeescape/Index'));
+Route::get('/refactoring', fn() => inertia('Refactoring/Index'));
+Route::get('/inertia', fn() => inertia('Inertia/Index'));
