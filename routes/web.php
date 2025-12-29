27dcd3dc1c2c1c8af5ab7f01431844439aa4507b
flm_10001 <firstlinuxmint@gmail.com>
2025-12-29 18:01:33 +0900

    feat: 各学習ページへのルーティングとホーム画面の更新
    - routes/web.php に各トピックのルート定義を追加
    - welcome.blade.php を学習ガイドのポータル画面に刷新。

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\BladeEscapeController;
+use App\Http\Controllers\ErrorFixController;
+use App\Http\Controllers\QueryBuilderController;
+use App\Http\Controllers\RefactoringController;
+use App\Http\Controllers\ValidationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/querybuilder', [QueryBuilderController::class, 'index']);
+Route::get('/querybuilder/guide', [QueryBuilderController::class, 'guide']);
+
+Route::get('/validation', [ValidationController::class, 'index']);
+Route::post('/validation/validate', [ValidationController::class, 'validate']);
+Route::get('/validation/guide', [ValidationController::class, 'guide']);
+
+Route::get('/errorfix', [ErrorFixController::class, 'index']);
+Route::get('/bladeescape', [BladeEscapeController::class, 'index']);
+Route::get('/refactoring', [RefactoringController::class, 'index']);
